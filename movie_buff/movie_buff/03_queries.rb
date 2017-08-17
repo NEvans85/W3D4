232a62105b153a612ac6cef867a3a0a8783ed887
App Academy Student <student@appacademy.io>
2017-08-17 15:19:35 -0700

    made progress on movie_buff, started pollsapp

--- movie_buff/movie_buff/03_queries.rb
+++ movie_buff/movie_buff/03_queries.rb
@@ -1,23 +1,41 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
-
+  Movie.select(:id, :title)
+    .joins(:actors)
+    .where(actors: { name: those_actors })
+    .group(:id)
+    .having("COUNT(*) >= #{those_actors.length}")
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
-
+  Movie
+    .group('decade')
+    .order('AVG(score) DESC')
+    .limit(1)
+    .pluck('yr/10 * 10 AS decade').first
 end
 
 def costars(name)
   # List the names of the actors that the named actor has ever
   # appeared with.
   # Hint: use a subquery
-
+  Actor
+    .joins(:castings)
+    .where(castings: { movie_id: Actor.select('castings.movie_id')
+                                      .joins(:castings)
+                                      .where(name: name)})
+    .where("name NOT LIKE '#{name}'")
+    .pluck(:name).uniq
 end
 
 def actor_out_of_work
   # Find the number of actors in the database who have not appeared in a movie
+  Actor
+    .joins('LEFT OUTER JOIN castings ON castings.actor_id = actors.id')
+    .where(castings: { movie_id: nil })
+    .pluck('COUNT(*)').first
 
 end
 
@@ -28,7 +46,8 @@ def starring(whazzername)
 
   # ex. "Sylvester Stallone" is like "sylvester" and "lester stone" but
   # not like "stallone sylvester" or "zylvester ztallone"
-
+  Movie
+    .joins(:actors)
 end
 
 def longest_career
